cc845c94fb4bf690c31ce1eebf50627c4dc38cff
Thomas Bush <tomb@khws.co.uk>
2018-01-30 11:05:56 +0000

    removed extra footer icons

--- src/partials/footer.php
+++ src/partials/footer.php
@@ -8,12 +8,6 @@
 
 		<a href="#" class="insta" name="Instagram"></a>
 
-		<a href="#" class="linked" name="LinkedIn"></a>
-
-		<a href="#" class="twitter" name="Twitter"></a>
-
-		<a href="#" class="insta" name="Instagram"></a>
-
 	</div>
 
 </footer>
